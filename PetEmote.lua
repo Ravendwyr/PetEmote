152f41f349cfdf0483fa0c8e5dba15d3af844d6b
Bert Kößler <morven@gmail.com>
2009-01-18 15:47:40 +0000

    Version 1.2.0

--- PetEmote.lua
+++ PetEmote.lua
@@ -1,16 +1,51 @@
-PetEmote_apos = "’";
+﻿PetEmote_apos = "’";
 PetEmote_nbsp = " ";
+
+PetEmote_Family = {};
 
 function PetEmote_OnLoad()
 	
 	SLASH_PETEMOTE1 = "/pet";
-	SlashCmdList["PETEMOTE"] = PetEmote_DoEmote;
+	SlashCmdList["PETEMOTE"] = PetEmote_Command;
 	
 end
 
-function PetEmote_DoEmote(text)
+function PetEmote_Command (args)
 	
-	if (HasPetUI() and UnitName("pet") ~= nil and UnitName("pet") ~= UNKNOWNOBJECT) then
+	local cmd, val = PetEmote_GetCommand(args);
+	
+	if (cmd == "family") then
+		if (PetEmote_HasPet()) then
+			if (val == "reset") then
+				PetEmote_ChangeFamily(UnitName("pet"), UnitCreatureFamily("pet"));
+			else
+				PetEmote_ChangeFamily(UnitName("pet"), val);
+			end
+		end
+	else
+		PetEmote_DoEmote(args);
+	end
+	
+end
+
+function PetEmote_GetCommand (args)
+ 	if args then
+		local a, b, c = string.find(args, "(%S+)");
+		if a then
+			return c, string.sub(args, b + 2);
+		else	
+			return "";
+		end
+	end
+end
+
+function PetEmote_HasPet ()
+	return (HasPetUI() and UnitName("pet") ~= nil and UnitName("pet") ~= UNKNOWNOBJECT);
+end
+
+function PetEmote_DoEmote (text)
+	
+	if (PetEmote_HasPet()) then
 		
 		if (string.sub(UnitName("player"), string.len(UnitName("player"))) == "s") then
 			nameAdd = PetEmote_apos;
@@ -18,11 +53,23 @@ function PetEmote_DoEmote(text)
 			nameAdd = PetEmote_apos .. 's';
 		end
 		
-		SendChatMessage(nameAdd .. PetEmote_nbsp .. UnitCreatureFamily("pet") .. PetEmote_nbsp .. UnitName("pet") .. PetEmote_nbsp .. text, "EMOTE");
+		if (PetEmote_Family[UnitName("pet")] ~= nil) then
+			family = PetEmote_Family[UnitName("pet")]
+		else
+			family = UnitCreatureFamily("pet");
+		end
+		
+		SendChatMessage(nameAdd .. PetEmote_nbsp .. family .. PetEmote_nbsp .. UnitName("pet") .. PetEmote_nbsp .. text, "EMOTE");
 		
 	end
 	
 end
+
+function PetEmote_ChangeFamily (pet, family)
+	PetEmote_Family[pet] = family;
+	DEFAULT_CHAT_FRAME:AddMessage(pet .. " = " .. family);
+end
+
 
 PetEmote_old_ChatFrame_OnEvent = ChatFrame_OnEvent;
 
