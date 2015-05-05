f53852a85d59ae8b4db989f3ef213ff9eec16e5c
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2015-05-05 20:24:33 +0100

    Bump version number to 1.8.7.

--- Globals.lua
+++ Globals.lua
@@ -27,7 +27,7 @@ PetEmote_CombatEmotes  = {}
 PetEmote_apos = "’"
 PetEmote_nbsp = " "
 
-PetEmote_Version = { 1, 8, 6 }
+PetEmote_Version = { 1, 8, 7 }
 
 PetEmote_Family = {}
 PetEmote_Gender = {}
